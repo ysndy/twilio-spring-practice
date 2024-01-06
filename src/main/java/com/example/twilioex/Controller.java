db86020bf85bd31889b7c7e279c296af0ed44ed3
ysndy <ysndy@naver.com>
2024-01-07 01:43:33 +0900

    전화번호 추출 가능한지 확인

--- src/main/java/com/example/twilioex/Controller.java
+++ src/main/java/com/example/twilioex/Controller.java
@@ -15,12 +15,17 @@ import java.io.IOException;
 public class Controller {
 
     @PostMapping("/voice")
-    public void incomingCall(HttpServletResponse response){
-        Say say = new Say.Builder("안녕하세요").language(Say.Language.KO_KR).build();
-
+    public void incomingCall(HttpServletRequest request, HttpServletResponse response){
+        Say say = new Say.Builder("안녕하세요 휠차차입니다. 전동휠체어 지도를 보내드리니 메시지를 확인해주세요. 감사합니다.").language(Say.Language.KO_KR).build();
         VoiceResponse twiml = new VoiceResponse.Builder().say(say).build();
+
         response.setContentType("text/xml");
         response.setCharacterEncoding("UTF-8");
+
+        //메시지 보내기
+        System.out.println(request.toString());
+        System.out.println(request.getParameter("From"));
+
         try {
             response.getWriter().print(twiml.toXml());
         } catch (TwiMLException | IOException e) {
